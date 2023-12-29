7726cec56c9d0c8bd261caa9f8d284bcf25ce7f1
tinaghimire <ghimirekristina10@gmail.com>
2023-12-29 10:56:18 +0545

    NLP added

--- Research/NLP/tokenize.py
+++
@@ -1,7 +0,0 @@
-# from nltk.tokenize import regexp_tokenize
-# from nltk.tokenize import TweetTokenizer
-
-from nltk.tokenize import word_tokenize
-
-# Split the string into tokens: tokens
-print(word_tokenize("Hi there"))
