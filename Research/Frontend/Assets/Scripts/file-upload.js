707afeb372cd58ffd5bc4135a9590b1bee522943
Pradeepti Dongol <pradeepti.d18@gmail.com>
2024-02-17 16:18:22 +0545

    Modify form

--- Research/Frontend/Assets/Scripts/file-upload.js
+++ Research/Frontend/Assets/Scripts/file-upload.js
@@ -1,34 +1,14 @@
-// // For all while ensuring total sum=100
-// document.addEventListener("DOMContentLoaded", function() {
-//     const sliders = document.querySelectorAll('.slider');
-//     const values = document.querySelectorAll('.value');
-
-//     sliders.forEach((slider, index) => {
-//         slider.addEventListener('input', function() {
-//             // Calculate the remaining sum
-//             const currentSum = Array.from(sliders).reduce((sum, s, i) => (i !== index ? sum + parseInt(s.value, 10) : sum), 0);
-
-//             // Update the max values of all sliders to ensure the total sum is 100
-//             const remainingMax = 100 - currentSum;
-//             sliders.forEach(s => (s.max = remainingMax));
-
-//             // Update the displayed value
-//             values[index].textContent = this.value;
-//         });
-//     });
-// });
-
 document.addEventListener("DOMContentLoaded", function() {
     const sliders = document.querySelectorAll('.slider');
     const values = document.querySelectorAll('.value');
-    const submitButton = document.querySelectorAll('.submit')
+    const submitButton = document.querySelector('.submit')
+    const errorMessage = document.getElementById('form-error-message');
     
-    sliders.forEach((slider, index) => 
-    {
+    sliders.forEach((slider, index) => {
         slider.addEventListener('input', function() {
 
             // Calculate the remaining sum
-            const currentSum = Array.from(sliders).reduce((sum, s, i) => (i !== index ? sum + parseInt(s.value, 10) : sum), 0);
+            const currentSum = Array.from(sliders).reduce((sum, s, i) => (i !== index ? sum + parseInt(s.value, 10): sum), 0);
 
             // Update the max values of all sliders to ensure the total sum is 100
             const remainingMax = 100 - currentSum;
@@ -36,12 +16,20 @@ document.addEventListener("DOMContentLoaded", function() {
 
             // Update the displayed value
             values[index].textContent = this.value;
+
+            // Prevent the user from sliding the sliders beyond their updated maximum values
+            // if (parseInt(this.value) > remainingMax) {
+            //     this.value = remainingMax;
+            //     values[index].textContent = remainingMax;
+            // }
+
+            if (currentSum === 100) {
+                submitButton.disabled = false;
+                errorMessage.textContent = "";
+            } else {
+                submitButton.disabled = true;
+                errorMessage.textContent = "The sum should be 100"
+            }
         });
     });
-
-    if (currentSum === 100) {
-            submitButton.disabled = false;
-        } else {
-            submitButton.disabled = true;
-        }
 });
